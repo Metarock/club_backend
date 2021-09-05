c7e3d73280a2da77f0e818de9d662e117c8b7893
Metarock <esang037@gmail.com>
2021-09-05 16:07:14 +1200

    edited azure

--- dist/resolvers/page.js
+++ dist/resolvers/page.js
@@ -26,6 +26,7 @@ const Page_1 = require("../entities/Page");
 const type_graphql_1 = require("type-graphql");
 const isAuth_1 = require("../middleware/isAuth");
 const FieldError_1 = require("../shared/FieldError");
+const typeorm_1 = require("typeorm");
 let PageInput = class PageInput {
 };
 __decorate([
@@ -104,6 +105,21 @@ let PageResolver = class PageResolver {
             }).save();
         });
     }
+    editPage(id, pageTitle, pageText, aboutUs, { req }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const result = yield (0, typeorm_1.getConnection)()
+                .createQueryBuilder()
+                .update(Page_1.Page)
+                .set({ pageTitle, pageText, aboutUs })
+                .where('id = :id and "creatorId" = :creatorId', {
+                id,
+                creatorId: req.session.userId
+            })
+                .returning("*")
+                .execute();
+            return result.raw[0];
+        });
+    }
     deletePage(id, { req }) {
         return __awaiter(this, void 0, void 0, function* () {
             yield Page_1.Page.delete({ id, creatorId: req.session.userId });
@@ -136,6 +152,18 @@ __decorate([
     __metadata("design:paramtypes", [Object, String, String, String, String]),
     __metadata("design:returntype", Promise)
 ], PageResolver.prototype, "createPage", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => Page_1.Page, { nullable: true }),
+    (0, type_graphql_1.UseMiddleware)(isAuth_1.isAuth),
+    __param(0, (0, type_graphql_1.Arg)('id', () => type_graphql_1.Int)),
+    __param(1, (0, type_graphql_1.Arg)('pageTitle')),
+    __param(2, (0, type_graphql_1.Arg)('pageText')),
+    __param(3, (0, type_graphql_1.Arg)('aboutUs')),
+    __param(4, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, String, String, String, Object]),
+    __metadata("design:returntype", Promise)
+], PageResolver.prototype, "editPage", null);
 __decorate([
     (0, type_graphql_1.Mutation)(() => Boolean),
     (0, type_graphql_1.UseMiddleware)(isAuth_1.isAuth),
