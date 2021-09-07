b22027360d2612dd5a153889884e43a227acd405
Metarock <esang037@gmail.com>
2021-09-08 09:17:50 +1200

    fixed session

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
+import { Session, SessionData } from "express-session";
 import { Redis } from "ioredis";
 import { userLoader } from "./utils/userLoader";
 
 
 export type MyContext = {
-    req: Request & { session: Express.Session & { userId?: number, pageId?: number } };
+    req: Request & { session: Session & Partial<SessionData> & { userId?: number, pageId?: number } };
     res: Response;
     redis: Redis;
     userLoader: ReturnType<typeof userLoader>;
