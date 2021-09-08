b2d7d0a2dc80837d92963a5e647382c42df96f3d
Metarock <esang037@gmail.com>
2021-09-08 12:09:46 +1200

    different domain

--- dist/index.js
+++ dist/index.js
@@ -70,8 +70,9 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         cookie: {
             maxAge: 1000 * 60 * 60,
             httpOnly: true,
+            sameSite: "lax",
             secure: process.env.NODE_ENV === "production" ? true : false,
-            domain: process.env.NODE_ENV === "production" ? 'azurewebsites.net' : undefined,
+            domain: process.env.NODE_ENV === "production" ? 'theclub-backend.azurewebsites.net' : undefined,
         },
         saveUninitialized: false,
         secret: process.env.SESSION_SECRET,
--- src/index.ts
+++ src/index.ts
@@ -71,8 +71,9 @@ const main = async () => {
             cookie: {
                 maxAge: 1000 * 60 * 60, //cookie durations
                 httpOnly: true,
+                sameSite: "lax",
                 secure: process.env.NODE_ENV === "production" ? true : false,
-                domain: process.env.NODE_ENV === "production" ? 'azurewebsites.net' : undefined,
+                domain: process.env.NODE_ENV === "production" ? 'theclub-backend.azurewebsites.net' : undefined,
 
             },
             saveUninitialized: false,
