38bfb8fc89466b232bebaae624ede6d4b8ad18a1
Metarock <esang037@gmail.com>
2021-09-20 19:06:54 +1200

    sameSite as none

--- src/index.ts
+++ src/index.ts
@@ -71,7 +71,7 @@ const main = async () => {
             cookie: {
                 maxAge: 1000 * 60 * 60, //cookie durations
                 httpOnly: true,
-                sameSite: 'lax',
+                sameSite: 'none',
                 secure: process.env.NODE_ENV === "production" ? true : false,
                 domain: process.env.NODE_ENV === "production" ? 'theclub-backend.azurewebsites.net' : undefined,
 
@@ -90,7 +90,7 @@ const main = async () => {
         context: ({ req, res }) => ({ req, res, redis, userLoader: userLoader(), postLoader: createPostLoader() }),
         playground: {
             settings: {
-                "request.credentials": "include",
+                "request.credentials": "include"
             }
         },
         introspection: true,
